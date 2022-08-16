61a0a46623bef89271d9013cc25d92f27598c079
tfkhdyt <tfkhdyt@pm.me>
2022-08-16 09:50:25 +0700

    feat: add tugas 6

---
+++ Tugas6/index.php
@@ -0,0 +1,70 @@
+<?php
+function penjumlahan($a, $b)
+{
+  return $a + $b;
+}
+
+function pengurangan($a, $b)
+{
+  return $a - $b;
+}
+
+function perkalian($a, $b)
+{
+  return $a * $b;
+}
+
+function pembagian($a, $b)
+{
+  return $a / $b;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tugas 6</title>
+  <!-- <link rel="stylesheet" href="style.css"> -->
+  <link rel="stylesheet" href="build.css">
+</head>
+
+<body>
+  <main class="grid place-items-center h-screen w-screen container mx-auto  ">
+    <section class="flex space-x-6 py-6">
+      <form class="bg-sky-100 p-6 pb-8 shadow hover:shadow-lg transition duration-300 flex flex-col space-y-4 rounded-lg" action="" method="POST">
+        <p class="font-bold text-lg text-center bg-gradient-to-r from-blue-700 to-red-500 text-transparent bg-clip-text">Aplikasi Kalkulator Sederhana</p>
+        <div class="flex space-x-4 items-center justify-around">
+          <label for="bilangan1" class="font-medium text-slate-700">Bilangan 1</label>
+          <input type="number" id="bilangan1" name="bilangan1" class="border border-slate-400 focus:ring-2 focus:ring-blue-500 outline-none px-2 py-1 w-4/6 rounded transition duration-300" />
+        </div>
+        <div class="flex space-x-4 items-center justify-around">
+          <label for="bilangan2" class="font-medium text-slate-700">Bilangan 2</label>
+          <input type="number" id="bilangan2" name="bilangan2" class="border border-slate-400 focus:ring-2 focus:ring-blue-500 outline-none px-2 py-1 w-4/6 rounded transition duration-300" />
+        </div>
+        <button class="bg-green-400 text-white font-bold py-2 rounded outline-2 outline-green-500 hover:bg-green-500 active:bg-green-600 transition duration-300" type="submit">Hitung</button>
+      </form>
+      <?php if (isset($_POST["bilangan1"]) && isset($_POST["bilangan2"])) :
+        $bilangan1 = $_POST["bilangan1"];
+        $bilangan2 = $_POST["bilangan2"];
+      ?>
+        <section class="bg-sky-100 p-6 pb-8 shadow hover:shadow-lg transition duration-300 flex flex-col space-y-4 rounded-lg w-72 text-slate-700 font-medium">
+          <p class="font-bold text-lg text-center bg-gradient-to-r from-blue-700 to-red-500 text-transparent bg-clip-text">Output</p>
+          <div class="tracking-widest leading-loose">
+            <p><?= "$bilangan1 + $bilangan2 = " . penjumlahan($bilangan1, $bilangan2) ?></p>
+            <p><?= "$bilangan1 - $bilangan2 = " . pengurangan($bilangan1, $bilangan2) ?></p>
+            <p><?= "$bilangan1 * $bilangan2 = " . perkalian($bilangan1, $bilangan2) ?></p>
+            <p><?= "$bilangan1 / $bilangan2 = " . number_format(pembagian($bilangan1, $bilangan2), 2) ?></p>
+          </div>
+          </div>
+        </section>
+      <?php endif; ?>
+    </section>
+  </main>
+</body>
+
+
+</html>
