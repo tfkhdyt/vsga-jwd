c07c7743f819fded924e70b5c33fe538f28dd172
tfkhdyt <tfkhdyt@pm.me>
2022-08-17 10:13:44 +0700

    chore: recreate the whole repo

---
+++ live_session_1/Tugas1_TaufikHidayat.php
@@ -0,0 +1,6 @@
+<?php
+echo "Nama: Taufik Hidayat <br>";
+echo "Sekolah: SMKN 7 Baleendah & Universitas Bale Bandung <br>";
+echo "No. WA: 0895338865375 <br>";
+echo "Alamat: Bandung <br>";
+echo "Alasan mengikuti pelatihan: Agar mendapatkan sertifikat kompetensi";
---
+++ live_session_1/hello_world.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hello World";
+$x = "Selamat Datang";
+$y = 5;
+
+echo "<br>";
+echo "Ini variabel x = " . $x . "<br>";
+echo "Ini variabel y = " . $y . "<br>";
+echo $x . "<br>";
+echo $y . "<br>";
