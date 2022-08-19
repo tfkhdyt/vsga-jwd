2b234c9d69d550839bafe645f0a43a94d5f7f8c4
tfkhdyt <tfkhdyt@pm.me>
2022-08-19 09:05:23 +0700

    feat(live_session_2): add tugas 2

---
+++ live_session_2/Tugas2_TaufikHidayat.php
@@ -0,0 +1,24 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tugas 3 | <?= $me["name"] ?></title>
+</head>
+
+<body>
+  <?= "==================== CETAK BILANGAN GANJIL GENAP 1 - 100 ====================" ?><br>
+  <?php
+  // atur nilai variable i dengan 1. lakukan perulangan sampai variable i sama dengan 100, increment variable i dengan 1 setiap setelah dieksekusi
+  for ($i = 1; $i <= 100; $i++) {
+    if ($i % 2 == 0) { // apabila variable i dibagi 2 sisa baginya sama dengan nol
+      echo "$i adalah Bilangan Genap <br>"; // maka angka tersebut genap
+    } else { // apabila tidak
+      echo "$i adalah Bilangan Ganjil <br>"; // maka angka tersebut ganjil
+    }
+  }
+  ?>
+</body>
+
+</html>
