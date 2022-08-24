d5d18c46e7ce34342386da2e385c7f299e9f2d23
tfkhdyt <tfkhdyt@pm.me>
2022-08-24 20:43:09 +0700

    feat: add live session 6

---
+++ live_session_6/Tugas6_TaufikHidayat/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'vendor/autoload.php';
+$factory = new RandomLib\Factory;
+$generator = $factory->getGenerator(new SecurityLib\Strength(SecurityLib\Strength::MEDIUM));
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Random Password</title>
+</head>
+
+<body>
+  <h1>Random Password Generator</h1>
+  <?php for ($i = 0; $i < 10; $i++) : ?>
+    <ul>
+      <li><?= $generator->generateString(32, 'abcdefg12345') . '\n' ?></li>
+    </ul>
+  <?php endfor ?>
+</body>
+
+</html>
